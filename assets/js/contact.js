525b8b46b7cde8a615ad9b39abd9caf7aa004fcb
Aurelie LB <aurelie.lebouguennec@gmail.com>
2018-11-13 14:41:50 +0100

    add checks form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,21 +1,46 @@
 (function () {
+  const oCaptchaChecker = {
+    hasRecaptcha: false,
+    recaptchaField: null,
+    checkGRecaptcha: function () {
+      if (this.hasRecaptcha === false) {
+        this.initGRecaptcha()
+      }
+      return this.isRecaptchaValid()
+    },
+    initGRecaptcha: function () {
+      this.recaptchaField = document.getElementById('g-recaptcha-response')
+      if (null === this.recaptchaField) {
+        this.recaptchaField = { validity: { valid: false } }
+      } else if (this.isRecaptchaValid() === false) {
+        this.hasRecaptcha = true
+        this.recaptchaField.addEventListener('change', (event) => {
+          if (this.isRecaptchaValid()) {
+            this.activateSubmitButton()
+          }
+        })
+      }
+    },
+    activateSubmitButton: function () {
+      document.getElementById('contactSubmit').removeAttribute('disabled')
+    },
+    isRecaptchaValid: function () {
+      return this.recaptchaField.validity.valid
+    },
+    checkAndActiveButton: function () {
+      if (this.checkGRecaptcha()) {
+        this.activateSubmitButton()
+      }
+    }
+  }
+
   const oForm = document.getElementById('contactForm')
   oForm.addEventListener('submit', function (event) {
-    if (false === checkGRecaptcha()) {
+    if (false === oCaptchaChecker.checkGRecaptcha()) {
       event.preventDefault()
     }
   })
   oForm.addEventListener('change', function (event) {
-    if (checkGRecaptcha()) {
-      activateSubmitButton()
-    }
+      oCaptchaChecker.checkAndActiveButton()
   })
-  function checkGRecaptcha() {
-    const recaptchaField = document.getElementById('g-recaptcha-response')
-    if (null === recaptchaField) return
-    return recaptchaField.validity.valid
-  }
-  function activateSubmitButton() {
-    document.getElementById('contactSubmit').removeAttribute('disabled')
-  }
 })()
